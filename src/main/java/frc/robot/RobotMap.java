7511625201be1a37141a79ad84ebdb96caea72a4
Dee <35465486+20iyerd@users.noreply.github.com>
2019-06-07 15:11:22 -0400

    *actually* copied drivetrain ids from 2019-robot

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -3,18 +3,18 @@ package frc.robot;
 public class RobotMap{
     //TODO: change IDs
     public static class Drivetrain{
-        public static final int LEFT_PRIMARY = 0;
-        public static final int LEFT_SECONDARY = 0;
-        public static final int LEFT_TERTIARY = 0;
+        public static final int LEFT_PRIMARY = 23;
+        public static final int LEFT_SECONDARY = 24;
+        public static final int LEFT_TERTIARY = 25;
 
-        public static final int RIGHT_PRIMARY = 0;
-        public static final int RIGHT_SECONDARY = 0;
-        public static final int RIGHT_TERTIARY = 0;
+        public static final int RIGHT_PRIMARY = 10;
+        public static final int RIGHT_SECONDARY = 11;
+        public static final int RIGHT_TERTIARY = 12;
     }
 
     public static class Powerpack{
-        public static final int PRIMARY = 0;
-        public static final int SECONDARY = 0;
+        public static final int PRIMARY = 13;
+        public static final int SECONDARY = 22;
     }
 
     public static class Sensors{

4db27eb86c3e56d7870e39ee31a81a4533b39c00
Dee <35465486+20iyerd@users.noreply.github.com>
2019-06-11 20:32:25 -0400

    added untested comments to motor controllers

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -1,20 +1,19 @@
 package frc.robot;
 
 public class RobotMap{
-    //TODO: change IDs
     public static class Drivetrain{
-        public static final int LEFT_PRIMARY = 23;
-        public static final int LEFT_SECONDARY = 24;
-        public static final int LEFT_TERTIARY = 25;
+        public static final int LEFT_PRIMARY = 23; // untested
+        public static final int LEFT_SECONDARY = 24; // untested
+        public static final int LEFT_TERTIARY = 25; // untested
 
-        public static final int RIGHT_PRIMARY = 10;
-        public static final int RIGHT_SECONDARY = 11;
-        public static final int RIGHT_TERTIARY = 12;
+        public static final int RIGHT_PRIMARY = 10; // untested
+        public static final int RIGHT_SECONDARY = 11; // untested
+        public static final int RIGHT_TERTIARY = 12; // untested
     }
 
     public static class Powerpack{
-        public static final int PRIMARY = 13;
-        public static final int SECONDARY = 22;
+        public static final int PRIMARY = 13; // untested
+        public static final int SECONDARY = 22; // untested
     }
 
     public static class Sensors{
@@ -37,8 +36,8 @@ public class RobotMap{
         public static final int MENU = 7;
 
         public static final int LX = 0;
-        public static final int LY = 1;
-        public static final int RX = 4;
+        public static final int LY = 1; // Arcade drive
+        public static final int RX = 4; // Arcade drive
         public static final int RY = 5;
         public static final int LT = 2;
         public static final int RT = 3;

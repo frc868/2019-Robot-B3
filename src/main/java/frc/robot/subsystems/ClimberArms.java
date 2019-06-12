afda3d7a33fe7ace5598bf612c9605836d4fe212
Dee <35465486+20iyerd@users.noreply.github.com>
2019-06-11 20:48:47 -0400

    Created ClimberArms class (former ramps)

---
+++ src/main/java/frc/robot/subsystems/ClimberArms.java
@@ -0,0 +1,21 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+/**
+ * This is the class for the former Ramps on the robot. The things on the front that help up climb.
+ * It was made a singleton to avoid multiple instantiations of the same sensor.
+ */
+public class ClimberArms {
+
+    private ClimberArms() {
+
+    }
+
+
+}

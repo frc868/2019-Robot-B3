7e7de1f42b52c8ee8c6b0c0d3bc0474946d7f4b8
Dee <35465486+20iyerd@users.noreply.github.com>
2019-06-13 14:07:12 -0400

    finished climber arms class

--- src/main/java/frc/robot/subsystems/ClimberArms.java
+++ src/main/java/frc/robot/subsystems/ClimberArms.java
@@ -13,14 +13,16 @@ import frc.robot.RobotMap;
 /**
  * This is the class for the former Ramps on the robot. The things on the front that help up climb.
  * It was made a singleton to avoid multiple instantiations of the same sensor.
+ * @author dri
  */
 public class ClimberArms {
 
     private static ClimberArms instance;
-    private Solenoid solenoid;
+    private Solenoid arms;
+    private final boolean DEPLOYED_STATE = true; //TODO: test deployed state for solenoid
 
     private ClimberArms() {
-        solenoid = new Solenoid(RobotMap.PCM, RobotMap.ClimberArms.SOLENOID_CHANNEL);
+        arms = new Solenoid(RobotMap.PCM, RobotMap.ClimberArms.SOLENOID_CHANNEL);
         // always specify PCM ID even if we don't have 2 PCMs, just to be safe.
     }
 
@@ -35,7 +37,38 @@ public class ClimberArms {
         }
         return instance;
     }
+    /**
+     * sets the state of the solenoid to deployed or secured
+     * true = deployed
+     * false = secured
+     * @param state the state to set it to
+     */
+    public void setState(boolean state) {
+        arms.set(state);
+    }
 
+    /**
+     * sets the state of the solenoid to true, which is the "deployed" state
+     */
+    public void deploy() {
+        arms.set(DEPLOYED_STATE);
+    }
+
+    /**
+     * sets the state of the solenoid to false, which is the "secured" state
+     */
+    public void secure() {
+        arms.set(!DEPLOYED_STATE);
+    }
     
+    /**
+     * returns the state of the climber arms solenoid
+     * if true, deployed
+     * if false, secured
+     * @return state of solenoid
+     */
+    public boolean getState() {
+        return arms.get();
+    }
 
 }

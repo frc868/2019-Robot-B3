8b6e3af0c8ca9fe895bd5b25409df429330cf742
Dee <35465486+20iyerd@users.noreply.github.com>
2019-11-03 18:37:23 -0500

    verified that hatchclaw works

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -9,7 +9,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  * Init should be called once in the robotInit() method in the Robot class
  * Update should be called either in robotPeriodic() or teleopPeriodic()
  * 
- * @author DRI
+ * @author dri
  */
 public class OI{
     public static XboxController driver = new XboxController(RobotMap.Controllers.DRIVER_PORT);
@@ -22,7 +22,7 @@ public class OI{
     public static void update() {
         Robot.drivetrain.arcadeDrive();
         if(operator.getRawButtonPressed(RobotMap.Controllers.LB)) {
-            Robot.hatchClaw.toggle(); //TODO: Sanity check on implementation of button reading
+            Robot.hatchClaw.toggle();
         }
         Robot.climberElevator.setSpeed(operator.getRawAxis(RobotMap.Controllers.LY));
 

e2da6d6e45d43c908e4658fef1e5839d31e2b809
Hazel Levine <rose.hazel@protonmail.ch>
2019-11-03 19:32:57 -0500

    standards fixes in OI

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -3,7 +3,6 @@ package frc.robot;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-
 /**
  * The class in which we map our driver/operator input to specific tasks on the robot
  * Init should be called once in the robotInit() method in the Robot class
@@ -11,7 +10,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  * 
  * @author dri
  */
-public class OI{
+public class OI {
     public static XboxController driver = new XboxController(RobotMap.Controllers.DRIVER_PORT);
     public static XboxController operator = new XboxController(RobotMap.Controllers.OPERATOR_PORT);
 
@@ -21,7 +20,7 @@ public class OI{
 
     public static void update() {
         Robot.drivetrain.arcadeDrive();
-        if(operator.getRawButtonPressed(RobotMap.Controllers.LB)) {
+        if (operator.getRawButtonPressed(RobotMap.Controllers.LB)) {
             Robot.hatchClaw.toggle();
         }
         Robot.climberElevator.setSpeed(operator.getRawAxis(RobotMap.Controllers.LY));
@@ -35,7 +34,5 @@ public class OI{
     public static void updateSD() {
         SmartDashboard.putBoolean("Climber Top Limit", Robot.climberElevator.getTopLimit());
         SmartDashboard.putBoolean("Climber Bot Limit", Robot.climberElevator.getBotLimit());
-
-
     }
 }

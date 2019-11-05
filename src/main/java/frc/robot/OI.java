8c167ee234aade663a331253b301568723e7a381
Hazel Levine <rose.hazel@protonmail.ch>
2019-11-05 15:51:54 -0500

    add tilt movement for climb and foot drive control

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -45,7 +45,7 @@ public class OI {
 
         driver.bMENU.whenPressed(() -> {
             Robot.climberArms.deploy();
-            // TODO: Set tilt position "Upper"
+            Robot.tilt.setPosition(TiltPosition.HIGH);
             Robot.manipulator.grab();
         });
         // TODO: Start -- ManualClimber
@@ -60,6 +60,9 @@ public class OI {
         operator.dS.whenPressed(() -> Robot.tilt.setPosition(TiltPosition.LOW));
 
         operator.bLB.whenPressed(() -> Robot.manipulator.toggle());
+
+        // TODO: not sure if this needs to check climb mode
+        Robot.climberElevator.driveFoot(operator.getRY());
 
         updateSD();
     }
